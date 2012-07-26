794a571da589ec8ab31cfe8d041c20662dbdd3de
Alejandro Castaño del Castillo <alex@wadobo.com>
2012-07-26 17:52:38 +0200

    Removing unnecessary comments

--- remote-control/main.cpp
+++ remote-control/main.cpp
@@ -10,11 +10,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
-//    QNetworkAccessManager networkManager;
-//    CommandManager commandManager(&networkManager);
     QmlApplicationViewer viewer;
-//    QDeclarativeContext *ctxt = viewer.rootContext();
-//    ctxt->setContextProperty("commandManager", &commandManager);
     qmlRegisterType<CommandManager>("com.wadobo.commandplugin", 1, 0, "CommandManager");
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/remotecontrol/main.qml"));
